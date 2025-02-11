ced9ab7fc897fb8f757b301d4fb809e77cee4b6c
Attla Mayank Rao <mayankatla13@gmail.com>
2025-02-11 11:02:17 +0530

    Create knowing_cv2module.py
    Captures images using the laptop camera (can be changed to other camera mode) and saves it. Each time it captures a new image the old one is replaced with the new one with the same name.

---
+++ knowing_cv2module.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+# Opens the laptop camera (as 0 is the default camera)
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Can't receive frame (stream end?). Exiting...")
+        break
+
+    cv2.imshow('Video Feed', frame)
+    img_counter=0
+
+    # Pressing 'q' on the keyboard will exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    elif cv2.waitKey(1) & 0xFF == ord('s'):
+        im=f"captured_image_{img_counter}.png"
+        path=os.path.join("C:/Users/mayan/Desktop",im)
+        cv2.imwrite(im,frame)
+        print("captured")
+        
+        img_counter=img_counter+1
+
+cap.release()
+cv2.destroyAllWindows()
